a7f17da0c09cc5817f9c529ef0eb379ce180f25e
yeojinia <kyj8765@gmail.com>
2020-12-01 10:41:32 -0800

    Update fast_sep_nmf.py

--- fast_sep_nmf.py
+++ fast_sep_nmf.py
@@ -1,9 +1,5 @@
 """
 Filename: 	    fast_sep_nmf.py
-Author:   	    Yeojin Kim
-Date:     	    June 2020
-Description:    This file is an implementation of original algorithm 1,
-"N. Gillis and S.A. Vavasis, Fast and Robust Recursive Algorithms for Separable Nonnegative Matrix Factorization"
 """
 
 from scipy.sparse import spdiags
